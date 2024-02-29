16300f4698afab57d614c0dfc5ad5c161cbaef74
mohdammar128 <ammarsiddiqui1282003@gmail.com>
2024-02-29 10:54:23 +0530

    user apis updated

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authentication.js");
-const { signIn, signUp, getAllDetailsOfUser } = require("../controllers/userController.js")
+const { signUp, getAllDetailsOfUser } = require("../controllers/userController.js")
 const { transferPoints } = require("../controllers/transaction.js")
 const router = express.Router();
 const txnMiddlewarec = require("../middleware/transactionMiddleware.js")
 
-router.post("/sign-up", signUp);
-router.post("/sign-in", signIn);
+
+// signUp 
+router.post("/user", authMiddleware, signUp);
+// get user details if its exist otherswise redirect them to signUp page based on backend status send to frontend 
 router.get('/:uid', getAllDetailsOfUser)
+// 
 router.put("/transfer-points", txnMiddlewarec, transferPoints);
 module.exports = router;
