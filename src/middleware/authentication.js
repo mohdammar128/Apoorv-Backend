191e2b5a08868b185a208af776ae1c3d25390d71
Ganesh Nathan <ganeshnathan06@gmail.com>
2024-03-17 21:31:22 +0530

    change middleware error messages

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -10,7 +10,7 @@ async function authMiddleware(request, response, next) {
     if (!decodedToken) {
       return res
         .status(403)
-        .send({ error: "not_authorized! signup first", success: false });
+        .send({ error: "Oops! It seems like you're not authorized for this request. Please try refreshing the page or signing in to the app again.", success: false });
     }
     const uid = decodedToken.uid;
     request.body["uid"] = uid;
@@ -18,7 +18,7 @@ async function authMiddleware(request, response, next) {
     next();
   } catch (error) {
     // console.log(error.message)
-    response.status(500).send({ error: "Token expired", success: false });
+    response.status(500).send({ error: "Your token has expired. Please sign in again.", success: false });
   }
 }
 
@@ -30,12 +30,12 @@ async function isUserExistMiddleware(req, res, next) {
       console.log("userId:", user._id);
       return res
         .status(409)
-        .send({ error: "User already exist", success: true, userId: user._id });
+        .send({ error: "User already exists.", success: true, userId: user._id });
     }
     next();
   } catch (error) {
     res.send(500).send({
-      error: "User validation failed ,please try again",
+      error: "User validation failed, please try again",
       success: false,
     });
   }
@@ -52,7 +52,7 @@ async function isShopAuthorized(req, res, next) {
     ) {
       return res.status(400).send({
         error:
-          "You are not an authorized shopkeeper. Please check your details.",
+          "Unauthorized: You are not recognized as a shopkeeper. Please review your details.",
         success: false,
       });
     }
@@ -63,7 +63,7 @@ async function isShopAuthorized(req, res, next) {
     res
       .status(500)
       .send({
-        error: "Authorization has failed. Please try again.",
+        error: "Authorization failed. Please try again.",
         success: false,
       });
   }
